5eae1b6bb65aacb906a5f8c7d91a0564c1bfa8fb
amine lgdi <amine.laaguidi.9@gmail.com>
2023-07-08 19:19:36 +0100

    Initial commit

---
+++ src/main/java/com/example/t2s/T2SApplication.java
@@ -0,0 +1,13 @@
+package com.example.t2s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2SApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2SApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/t2s/T2SApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.t2s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2SApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
