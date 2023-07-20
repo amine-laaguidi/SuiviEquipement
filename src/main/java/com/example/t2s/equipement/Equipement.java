c5974225f876d39fb964354a70ef466e61f01748
amine lgdi <amine.laaguidi.9@gmail.com>
2023-07-20 04:34:24 +0100

    equipement: certificat functions

--- src/main/java/com/example/t2s/equipement/Equipement.java
+++ src/main/java/com/example/t2s/equipement/Equipement.java
@@ -5,9 +5,10 @@ import com.example.t2s.certificat.Certificat;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.*;
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
-@Entity @Getter @Setter @AllArgsConstructor @NoArgsConstructor
+@Entity @Getter @Setter @AllArgsConstructor
 public class Equipement {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,6 +19,21 @@ public class Equipement {
     Integer gmdn;
     @OneToMany(mappedBy = "equipement",cascade = CascadeType.ALL) @JsonBackReference
     List<Accessoire> accessoires;
-    @OneToMany(mappedBy = "equipement",cascade = CascadeType.ALL) @JsonBackReference
+    @OneToMany(mappedBy = "equipement",cascade = CascadeType.ALL)
     List<Certificat> certificats;
+
+    public Equipement() {
+        idE=null;
+        designation=null;
+        classe=null;
+        gmdn=null;
+        accessoires=new ArrayList<>();
+        certificats = new ArrayList<>();
+    }
+
+
+    public void addCertificat(Certificat certificat){
+        certificats.add(certificat);
+        certificat.setEquipement(this);
+    }
 }
